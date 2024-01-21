3354124c1960b5c8fc274a556882bffad635d811
Georgi Pandev <georgi.pandev.33@gmail.com>
2024-01-21 12:10:34 +0200

    Add pages

---
+++ src/pages/BasketBall/BasketballPage.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const BasketballPage = () => {
+  return <div>BasketballPage</div>;
+};
---
+++ src/pages/Tennis/TennisPage.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const TennisPage = () => {
+  return <div>TennisPage</div>;
+};
